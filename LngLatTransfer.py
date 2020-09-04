d54fba7c634fd1d0755884765286e5e5ad14acd3
JoeEmp <hemingjie>
2020-09-04 16:52:31 +0800

    添加坐标距离计算工具

---
+++ LngLatTransfer.py
@@ -0,0 +1,196 @@
+'''
+@Author: your name
+@Date: 2020-02-27 14:05:09
+@LastEditTime: 2020-06-05 10:34:52
+@LastEditors: Please set LastEditors
+@Description: In User Settings Edit
+@FilePath: /test_script/api_sql/utils/LngLatTransfer.py
+'''
+import math
+from math import radians, cos, sin, asin, sqrt
+
+
+class LngLatTransfer():
+    """ WGS84、GCJ02（火星坐标系）、BD09（百度坐标系）以及百度地图中保存矢量信息的web墨卡托 """
+
+    def __init__(self):
+        self.x_pi = 3.14159265358979324 * 3000.0 / 180.0
+        self.pi = math.pi  # π
+        self.a = 6378245.0  # 长半轴
+        self.es = 0.00669342162296594323  # 偏心率平方
+        pass
+
+    def GCJ02_to_BD09(self, gcj_lng, gcj_lat):
+        """
+        实现GCJ02向BD09坐标系的转换
+        :param lng: GCJ02坐标系下的经度
+        :param lat: GCJ02坐标系下的纬度
+        :return: 转换后的BD09下经纬度
+        """
+        z = math.sqrt(gcj_lng * gcj_lng + gcj_lat * gcj_lat) + \
+            0.00002 * math.sin(gcj_lat * self.x_pi)
+        theta = math.atan2(gcj_lat, gcj_lng) + 0.000003 * \
+            math.cos(gcj_lng * self.x_pi)
+        bd_lng = z * math.cos(theta) + 0.0065
+        bd_lat = z * math.sin(theta) + 0.006
+        return bd_lng, bd_lat
+
+    def BD09_to_GCJ02(self, bd_lng, bd_lat):
+        '''
+        实现BD09坐标系向GCJ02坐标系的转换
+        :param bd_lng: BD09坐标系下的经度
+        :param bd_lat: BD09坐标系下的纬度
+        :return: 转换后的GCJ02下经纬度
+        '''
+        x = bd_lng - 0.0065
+        y = bd_lat - 0.006
+        z = math.sqrt(x * x + y * y) - 0.00002 * math.sin(y * self.x_pi)
+        theta = math.atan2(y, x) - 0.000003 * math.cos(x * self.x_pi)
+        gcj_lng = z * math.cos(theta)
+        gcj_lat = z * math.sin(theta)
+        return gcj_lng, gcj_lat
+
+    def WGS84_to_GCJ02(self, lng, lat):
+        '''
+        实现WGS84坐标系向GCJ02坐标系的转换
+        :param lng: WGS84坐标系下的经度
+        :param lat: WGS84坐标系下的纬度
+        :return: 转换后的GCJ02下经纬度
+        '''
+        dlat = self._transformlat(lng - 105.0, lat - 35.0)
+        dlng = self._transformlng(lng - 105.0, lat - 35.0)
+        radlat = lat / 180.0 * self.pi
+        magic = math.sin(radlat)
+        magic = 1 - self.es * magic * magic
+        sqrtmagic = math.sqrt(magic)
+        dlat = (dlat * 180.0) / ((self.a * (1 - self.es)) /
+                                 (magic * sqrtmagic) * self.pi)
+        dlng = (dlng * 180.0) / (self.a /
+                                 sqrtmagic * math.cos(radlat) * self.pi)
+        gcj_lng = lat + dlat
+        gcj_lat = lng + dlng
+        return gcj_lng, gcj_lat
+
+    def GCJ02_to_WGS84(self, gcj_lng, gcj_lat):
+        '''
+        实现GCJ02坐标系向WGS84坐标系的转换
+        :param gcj_lng: GCJ02坐标系下的经度
+        :param gcj_lat: GCJ02坐标系下的纬度
+        :return: 转换后的WGS84下经纬度
+        '''
+        dlat = self._transformlat(gcj_lng - 105.0, gcj_lat - 35.0)
+        dlng = self._transformlng(gcj_lng - 105.0, gcj_lat - 35.0)
+        radlat = gcj_lat / 180.0 * self.pi
+        magic = math.sin(radlat)
+        magic = 1 - self.es * magic * magic
+        sqrtmagic = math.sqrt(magic)
+        dlat = (dlat * 180.0) / ((self.a * (1 - self.es)) /
+                                 (magic * sqrtmagic) * self.pi)
+        dlng = (dlng * 180.0) / (self.a /
+                                 sqrtmagic * math.cos(radlat) * self.pi)
+        mglat = gcj_lat + dlat
+        mglng = gcj_lng + dlng
+        lng = gcj_lng * 2 - mglng
+        lat = gcj_lat * 2 - mglat
+        return lng, lat
+
+    def BD09_to_WGS84(self, bd_lng, bd_lat):
+        '''
+        实现BD09坐标系向WGS84坐标系的转换
+        :param bd_lng: BD09坐标系下的经度
+        :param bd_lat: BD09坐标系下的纬度
+        :return: 转换后的WGS84下经纬度
+        '''
+        lng, lat = self.BD09_to_GCJ02(bd_lng, bd_lat)
+        return self.GCJ02_to_WGS84(lng, lat)
+
+    def WGS84_to_BD09(self, lng, lat):
+        '''
+        实现WGS84坐标系向BD09坐标系的转换
+        :param lng: WGS84坐标系下的经度
+        :param lat: WGS84坐标系下的纬度
+        :return: 转换后的BD09下经纬度
+        '''
+        lng, lat = self.WGS84_to_GCJ02(lng, lat)
+        return self.GCJ02_to_BD09(lng, lat)
+
+    def _transformlat(self, lng, lat):
+        ret = -100.0 + 2.0 * lng + 3.0 * lat + 0.2 * lat * lat + \
+            0.1 * lng * lat + 0.2 * math.sqrt(math.fabs(lng))
+        ret += (20.0 * math.sin(6.0 * lng * self.pi) + 20.0 *
+                math.sin(2.0 * lng * self.pi)) * 2.0 / 3.0
+        ret += (20.0 * math.sin(lat * self.pi) + 40.0 *
+                math.sin(lat / 3.0 * self.pi)) * 2.0 / 3.0
+        ret += (160.0 * math.sin(lat / 12.0 * self.pi) + 320 *
+                math.sin(lat * self.pi / 30.0)) * 2.0 / 3.0
+        return ret
+
+    def _transformlng(self, lng, lat):
+        ret = 300.0 + lng + 2.0 * lat + 0.1 * lng * lng + \
+            0.1 * lng * lat + 0.1 * math.sqrt(math.fabs(lng))
+        ret += (20.0 * math.sin(6.0 * lng * self.pi) + 20.0 *
+                math.sin(2.0 * lng * self.pi)) * 2.0 / 3.0
+        ret += (20.0 * math.sin(lng * self.pi) + 40.0 *
+                math.sin(lng / 3.0 * self.pi)) * 2.0 / 3.0
+        ret += (150.0 * math.sin(lng / 12.0 * self.pi) + 300.0 *
+                math.sin(lng / 30.0 * self.pi)) * 2.0 / 3.0
+        return ret
+
+    def WGS84_to_WebMercator(self, lng, lat):
+        '''
+        实现WGS84向web墨卡托的转换
+        :param lng: WGS84经度
+        :param lat: WGS84纬度
+        :return: 转换后的web墨卡托坐标
+        '''
+        x = lng * 20037508.342789 / 180
+        y = math.log(math.tan((90 + lat) * self.pi / 360)) / (self.pi / 180)
+        y = y * 20037508.34789 / 180
+        return x, y
+
+    def WebMercator_to_WGS84(self, x, y):
+        '''
+        实现web墨卡托向WGS84的转换
+        :param x: web墨卡托x坐标
+        :param y: web墨卡托y坐标
+        :return: 转换后的WGS84经纬度
+        '''
+        lng = x / 20037508.34 * 180
+        lat = y / 20037508.34 * 180
+        lat = 180 / self.pi * \
+            (2 * math.atan(math.exp(lat * self.pi / 180)) - self.pi / 2)
+        return lng, lat
+
+
+def haversine(lon1, lat1, lon2, lat2):  # 经度1，纬度1，经度2，纬度2 （十进制度数）
+    """  
+    Calculate the great circle distance between two points   
+    on the earth (specified in decimal degrees)
+    单位为米
+    case
+        m_latitude = 23.0955810546875
+        m_longitude = 113.28564453125
+        g_latitude = 39.630317
+        g_longitude = 116.332978
+        l = haversine(m_longitude, m_latitude, g_longitude, g_latitude)
+        print('高精度：{} M'.format(l))
+    """
+    # 将十进制度数转化为弧度
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine公式
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    r = 6371  # 赤道平均半径，单位为公里
+    return c * r * 1000 # 单位为米
+
+
+if __name__ == "__main__":
+    m_latitude = 23.0955810546875
+    m_longitude = 113.28564453125
+    g_latitude = 39.630317
+    g_longitude = 116.332978
+    l = haversine(m_longitude, m_latitude, g_longitude, g_latitude)
+    print('({},{}) 距 ({},{})：{} M'.format(m_longitude, m_latitude, g_longitude, g_latitude,l))

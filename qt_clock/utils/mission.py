3452091a2eced09001404a97ca6058a2f14c5af2
JoeEmp <hemingjie>
2020-09-22 11:09:57 +0800

    精简代码,和重命名函数

--- qt_clock/utils/mission.py
+++ qt_clock/utils/mission.py
@@ -22,54 +22,41 @@ class TimeMission():
 
     Attributes:
         missions:任务配置信息 dict of dict(为具体的任务)
-
     """
 
     def __init__(self):
         super().__init__()
-        self.missions = self.get_json()
+        self.missions = self.get_config()
 
-    def get_json(self, filename=MISSION_CONFIG) -> dict:
-        """从文件获取任务信息.
-
-        :param filename:配置文件，默认为 config文件的MISSION_CONFIG
-        :return: dict
-        """
+    def get_config(self, filename=MISSION_CONFIG) -> dict:
+        """get file config."""
         try:
-            logging.debug('filename is {}'.format(filename))
             with open(filename, encoding='utf-8') as f:
-                # logging.info('read job from json file')
                 return json.loads(f.read(), encoding='utf-8')
         except FileNotFoundError:
+            return dict()
+        except json.decoder.JSONDecodeError:
             return dict()
 
     def update_json(self, filename=MISSION_CONFIG) -> bool:
         try:
             with open(filename, 'w') as f:
-                f.write(json.dumps(self.missions))
+                f.write(json.dumps(self.missions, ensure_ascii=False))
                 return True
         except FileNotFoundError as e:
             logging.warning(e)
         return False
 
-    def set_mission(self, missions=None):
-        """设置定时任务
-
-        :param missions:任务配置
-        :return:
-        """
-        # set aps
+    def set_aps_mission(self, missions=None):
+        """设置定时任务. """
         if missions:
             self.missions = missions
         MYAPS.add_json_jobs(self.missions)
 
-    def add_mission(self, mission: dict, filename=MISSION_CONFIG):
-        if not self.missions:
-            self.missions = self.get_json(filename)
-            MYAPS.set_json_jobs(self.missions)
-        
+    def add_aps_mission(self, mission: dict, filename=MISSION_CONFIG):
         # add aps
-        MYAPS.add_json_job(mission)
+        if mission:
+            MYAPS.add_json_job(mission)
 
     def get_mission(self, mission_id: str) -> dict:
         try:
@@ -77,26 +64,22 @@ class TimeMission():
         except Exception as e:
             return dict()
 
-    def update_mission(self, mission: dict,flag: bool):
+    def update_mission(self, mission: dict, flag: bool):
         if flag:
             try:
                 mission_id = mission['id']
                 self.missions[mission_id] = mission
-                self.update_file()
+                self.update_config_file()
                 # update aps
                 MYAPS.update_mission(mission)
             except KeyError:
-                self.add_mission(mission)
+                self.add_aps_mission(mission)
 
     def del_mission(self, mission_id) -> bool:
-        """去除内存和aps以及文件的任务配置
-
-        :param mission_id:任务id
-        :return:
-        """
+        """去除内存和aps以及文件的任务配置. """
         try:
             self.missions.pop(mission_id)
-            self.update_file()
+            self.update_config_file()
             # remove aps job
             MYAPS.remove_job(mission_id)
             return True
@@ -104,9 +87,9 @@ class TimeMission():
             logging.warning(e)
         return False
 
-    def update_file(self,filename=MISSION_CONFIG):
+    def update_config_file(self, filename=MISSION_CONFIG):
         with open(filename, 'w') as f:
-            f.write(json.dumps(self.missions))
+            f.write(json.dumps(self.missions, ensure_ascii=False))
 
 
 TIME_MISSION = TimeMission()
